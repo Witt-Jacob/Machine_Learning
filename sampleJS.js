d0a3fe681bd402e4648a24a1a701b2c3919b13db
Jacob Witt <55113315+Witt-Jacob@users.noreply.github.com>
2022-07-14 15:29:04 -0600

    Add files via upload

---
+++ sampleJS.js
@@ -0,0 +1,18 @@
+//
+//  sampleJS.swift
+//  FormIoTest
+//
+//  Created by Miguel Figueroa on 7/14/22.
+//
+
+import JavaScriptCore
+
+//function toggleFocus(el)
+// {
+//  el.style.backgroundColor =  el.style.backgroundColor=="yellow" ? "inherit" : "yellow";
+// }
+//return document.getElementsByTagName('*');
+document.getElementsByClassName("form-group has-feedback formio-component formio-component-hidden formio-component-uuid uuid formio-component-label-hidden").innerHTML = hello;
+    
+//document.getElementsByClassName("col-form-label").innerHTML = hello;
+
